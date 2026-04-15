c9dc9ea03aad3d945ff9603e42384f1bb50cd93e
Youssef Menjour <71812679+Ymenjour@users.noreply.github.com>
2026-04-15 20:10:58 +0200

    Create execute.hpp

---
+++ Implementations/Reference/Runtime/cpp/include/execute.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "runtime.hpp"
+
+namespace frog::runtime::cpp_ref {
+
+runtime_state execute_example05(const example05_contract& contract);
+
+} // namespace frog::runtime::cpp_ref
